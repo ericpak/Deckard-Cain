6c36fbf23aef203cd7e2a30759b54b25f900a481
Eric Pak <pak.eric@gmail.com>
2018-03-27 05:10:13 -0700

    easteregg

--- cmds/speak.js
+++ cmds/speak.js
@@ -1,14 +1,18 @@
 const cainVoice = __dirname + "/../media/audio/cain.mp3";
 const hello = "/home/eric/git/Deckard-Cain/cmds/hello.mp3";
+var audio = cainVoice;
+
 module.exports.run = async (bot, message, args) => {
-  console.log("HERRROOOO ");
   if(!message.member.voiceChannel){
     message.channel.send("Whhaattttt? I can't hear you! Maybe if you were in a voice channel i could hear you!");
     return;
   }
+  if(args[1] == "hellodarkness")
+    audio = hello;
+
   var voiceChannel = message.member.voiceChannel;
   voiceChannel.join().then(connection => {
-    const dispatcher = connection.playFile(cainVoice);
+    const dispatcher = connection.playFile(audio);
     dispatcher.on("end", end => {
       voiceChannel.leave();
     });
