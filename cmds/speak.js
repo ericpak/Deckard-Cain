e75a5af6f46d2150ee0f83424f2a81a2d4e01fe1
Eric Pak <pak.eric@gmail.com>
2018-03-27 05:35:04 -0700

    added more audio files

--- cmds/speak.js
+++ cmds/speak.js
@@ -1,14 +1,21 @@
 const cainVoice = __dirname + "/../media/audio/cain.mp3";
+const anya = __dirname + "/../media/audio/anya.mp3";
+const stay = __dirname + "/../media/audio/stay.mp3";
 const hello = __dirname + "/../media/audio/hello.mp3";
 
 module.exports.run = async (bot, message, args) => {
   var audio = cainVoice;
+  var chooser = Math.random();
   if(!message.member.voiceChannel){
     message.channel.send("Whhaattttt? I can't hear you! Maybe if you were in a voice channel i could hear you!");
     return;
   }
   if(args[0] == "hello" && args[1] == "darkness")
     audio = hello;
+  else if(chooser > 0.9)
+    audio = anya;
+  else if(chooser > 0.5)
+    audio = stay;
 
   var voiceChannel = message.member.voiceChannel;
   voiceChannel.join().then(connection => {
